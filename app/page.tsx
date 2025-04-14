e79b042a11e00b5ded34a5e08bd61f4a051c3ef2
brandvdo <brandvdo@gmail.com>
2025-04-14 12:18:43 -0500

    Fixed random int

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
+"use client";
+
 import dynamic from "next/dynamic";
+import { useState } from "react";
 
 export default function Home() {
-  const random = Math.random() < .5;
+  const [random] = useState(() => Math.random() < 0.5 ? 0 : 1);
+
+
   const PageComponent = dynamic(() =>
-    random ? import("./light") : import("./dark")
+    random === 1 ? import("./light") : import("./dark")
   );
 
-  return (
-    <>
-      <p>Random value: {random ? 1 : 0}</p>
-      <PageComponent />
-    </>
-  );
+  return <PageComponent />;
 }
